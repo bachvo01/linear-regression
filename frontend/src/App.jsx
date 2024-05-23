f27a26596e5eaa2fb596a965709a6764a32168a9
BachVo <bachvo01@gmail.com>
2024-05-23 21:52:50 +0700

    UIS-02: add fetch API from server; add prediction demo; add visualisation of fitting the regression line

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.scss'
+import RegressionGraph from './RegressionGraph';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [yoe, setYoE] = useState(0);
+  const [salary, setSalary] = useState(null);
 
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/predict", {
+        years_of_experience : parseFloat(yoe),
+      });
+      setSalary(response.data.predicted_salary)
+    } catch (error) {
+      console.error('Error...!', error);
+    }
+  }
+
+  useEffect(() => {
+    console.log(salary)
+  }, [salary])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='home-container'>
+      <div className='home-inner'>
+        <div className='header-wrapper'>
+          <h1 className='header monospace-header'>Linear Regression Model</h1>
+          <div className='info-container'>
+            <h2 className='info monospace-regular'>May 24, 2024. By Bach Vo</h2>
+          </div>
+        </div>
+        <div className='sep-l'></div>
+        <div className='article-container'>
+          <div className='overview-section'>
+            <h1 className='monospace-header-m'>Overview</h1>
+            <div className='sep-s'></div>
+            <p className='monospace-regular-p'>This project was created with the goal is to develop a predictive model for <strong>salary</strong> using 
+            <strong> linear regression</strong>, a fundamental statistical method, to identify the relationship between an individual's salary and their respective years of experience. <br></br><br></br>As the number of layoff announcements has massively increased since 2022, a big question that every job seekers must ask is what is the most suitable job worth pursuing in the market.
+            <br></br><br></br>Having a reliable model to predict job salary may become handy for job seekers before embarking on the journey of finding the best employment.
+            </p>
+          </div>
+
+          <div className='overview-section'>
+            <h1 className='monospace-header-m'>Dataset</h1>
+            <div className='sep-s'></div>
+            <p className='monospace-regular-p'>The project will utilize a dataset containing information about various factors that influence job salary, such as age, gender, education level, job title and especially years of experience. The dataset will be sourced from publicly available data sources (<a className='link' href='https://www.kaggle.com/datasets/mohithsairamreddy/salary-data'>Kaggle</a>).</p>
+          </div>
+          
+          <div className='demo-section'>
+            <h1 className='monospace-header-m'>Demonstration</h1>
+            <div className='sep-s'></div>
+            <form className='demo-container' onSubmit={handleSubmit}>
+                <div className='row'>
+                    <label className='monospace-regular-p'>Years of Experience</label>
+                    <input type='number' value={yoe} className='monospace-regular-p' onChange={((e) => setYoE(e.target.value))}></input>
+                </div>
+                {
+                  salary && <h2 className='monospace-regular-p'>Your predicted salary would be <span className='monospace-regular'>${Math.round(salary * 100) / 100}</span></h2>
+                }
+                <button className='monospace-regular-p' type='submit'>Predict salary</button>
+            </form>
+          </div>
+
+          <div className='overview-section'>
+            <h1 className='monospace-header-m'>Graph of the Regression Line</h1>
+            <div className='sep-s'></div>
+            <p className='monospace-regular-p'>Visualizing the regression line provides a clear understanding of how well the model fits the data. The steps include:
+              <br></br>- Scatter Plot: Plotting the original data points with years of experience on the x-axis and salary on the y-axis. <br></br>
+              - Regression Line: Adding the regression line to the scatter plot, which represents the predicted salary values for given years of experience.</p>
+            <div className='sep-l'></div>
+            <div className='sep-l'></div>
+            <RegressionGraph></RegressionGraph>
+          </div>
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ frontend/src/RegressionGraph.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from "react";
+import Papa from 'papaparse'
+import { Chart, Line, Scatter } from 'react-chartjs-2';
+import { Chart as ChartJS, LinearScale, PointElement, LineElement, Tooltip, Legend, plugins } from 'chart.js';
+import csv from './assets/data/Salary_Data.csv'
+
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+
+
+function RegressionGraph() {
+    const [data, setData] = useState([])
+    const [regressionLine, setRegressionLine] = useState([])
+
+    useEffect(() => {
+        fetch(csv)
+            .then(response => response.text())
+            .then(text => {
+                const parsedData = []
+                Papa.parse(text, {
+                    header: true,
+                    dynamicTyping: true,
+                    step: (results, parser) => {
+                        parsedData.push({
+                            x: results.data.YearsofExperience,
+                            y: results.data.Salary,
+                        })
+                        if(parsedData.length >= 500) {
+                            parser.abort();
+                            setData(parsedData)
+                            computeRegressionLine(parsedData)
+                        }
+                    },
+                    complete: () => {
+                        if (parsedData.length < 500) {
+                          setData(parsedData);
+                          computeRegressionLine(parsedData);
+                        }
+                      },
+                })
+            })
+    })
+
+    const computeRegressionLine = (data) => {
+        // Simple linear regression calculation
+        const n = data.length;
+        const sumX = data.reduce((sum, point) => sum + point.x, 0);
+        const sumY = data.reduce((sum, point) => sum + point.y, 0);
+        const sumXY = data.reduce((sum, point) => sum + point.x * point.y, 0);
+        const sumX2 = data.reduce((sum, point) => sum + point.x * point.x, 0);
+
+        const slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+        const intercept = (sumY - slope * sumX) / n;
+
+        const regressionLine = data.map((point) => ({
+        x: point.x,
+        y: intercept + slope * point.x,
+        }));
+        setRegressionLine(regressionLine);
+    }
+    // Plugin to customize background
+    const backgroundPlugin = {
+        id: 'customCanvasBackgroundColor',
+        beforeDraw: (chart) => {
+            const { ctx, width, height } = chart;
+            ctx.save();
+            ctx.fillStyle = '#3868A9'; // Set your desired background color here
+            ctx.fillRect(0, 0, width, height);
+            ctx.restore();
+        }
+    };
+
+
+    const chartData = {
+        datasets: [
+          {
+            label: 'Actual Data',
+            data: data.map((point) => ({ x: point.x, y: point.y })),
+            backgroundColor: '#A4B1CD',
+          },
+          {
+            label: 'Regression Line',
+            data: regressionLine.map((point) => ({ x: point.x, y: point.y })),
+            borderColor: '#9FEF00',
+            borderWidth: 2,
+            type: 'line',
+            fill: false,
+            showLine: true,
+          },
+        ],
+      };
+
+      const options = {
+        respnsive: true,
+        plugins: {
+            customCanvasBackgroundColor: backgroundPlugin,
+            legend: {
+                position: 'top',
+                labels: {
+                    font: {
+                        size: 14,
+                        family: 'monospace'
+                    },
+                    color: '#A4B1CD'
+                }
+            }
+        },
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom',
+            title: {
+              display: true,
+              text: 'Years of Experience',
+              color: '#A4B1CD'
+            },
+            ticks: {
+                color: '#A4B1CD'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Salary',
+              color: '#A4B1CD'
+            },
+            ticks: {
+                color: '#A4B1CD'
+            }
+          },
+        },
+      };
+    
+      return (
+        <div className="graph-container">
+          <Scatter data={chartData} options={options} />
+        </div>
+      );
+}
+
+export default RegressionGraph
