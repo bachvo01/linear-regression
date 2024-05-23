896b326dccf5fdfe32002e1947a4e284833d0cb3
BachVo <bachvo01@gmail.com>
2024-05-23 16:47:06 +0700

    APIS-02: add predict API endpoint, set server port to 5000.

--- server/server.py
+++ server/server.py
@@ -9,9 +9,19 @@ with open('salary_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Example input for prediction
-years_of_experience = np.array([[5]])
+# years_of_experience = np.array([[5]])
 
-# Make a prediction
-predicted_salary = model.predict(years_of_experience)
-print(f"Predicted Salary for 5 years of experience: ${predicted_salary[0]:.2f}")
+# # Make a prediction
+# predicted_salary = model.predict(years_of_experience)
+# print(f"Predicted Salary for 5 years of experience: ${predicted_salary[0]:.2f}")
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    years_of_experience = np.array([[data['years_of_experience']]])
+    prediction = model.predict(years_of_experience)
+    return jsonify({'predicted_salary' : prediction[0]})
+
+if __name__ == '__main__':
+    app.run(port=5000,debug=True)
+    print("app is running at PORT {port}")
