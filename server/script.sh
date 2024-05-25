3b99afb2c6bab17c6175e4b7576260e3ad34e4ee
BachVo <bachvo01@gmail.com>
2024-05-25 18:05:12 +0700

    APIS-06: add gunicorn for deployment; change script.sh file; add wsgi file.

--- server/script.sh
+++ server/script.sh
@@ -3,10 +3,11 @@ source myenv/bin/activate
 
 pip install -r requirements.txt
 
-pip install virtualenv
+pip install virtualenv gunicorn
 
 pip freeze > requirements.txt
 
-python server.py -m flask run --host=0.0.0.0
+# python server.py -m flask run --host=0.0.0.0
+gunicorn --bind 0.0.0.0:5000 wsgi:app
 
 deactivate
